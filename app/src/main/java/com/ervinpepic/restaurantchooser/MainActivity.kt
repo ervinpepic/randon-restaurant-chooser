cce14c16dd896b212b6a99fe9cab9825017205bf
Ervin Pepic <ervinpepic@ervin.local>
2018-02-03 00:40:54 +0100

    Added logic to app to work as expected

--- app/src/main/java/com/ervinpepic/restaurantchooser/MainActivity.kt
+++ app/src/main/java/com/ervinpepic/restaurantchooser/MainActivity.kt
@@ -3,13 +3,27 @@ package com.ervinpepic.restaurantchooser
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val foodlist = arrayListOf("Chinese", "German", "Mexican", "American", "Italian")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        selectedFoodTxt.text = "Burger King"
+        decide_button.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodlist.count())
+            selectedFoodTxt.text =  foodlist[randomFood]
+        }
+
+        addfood_button.setOnClickListener {
+            val newFood = addFoodText.text.toString()
+            foodlist.add(newFood)
+            addFoodText.text.clear()
+        }
+
     }
 }
